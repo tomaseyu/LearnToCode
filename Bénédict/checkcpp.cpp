9ad82575e821831f22bc2497bf2ce73921335a96
radaxsly <72070675+tomaseyu@users.noreply.github.com>
2022-11-10 08:47:20 +0100

    Create checkcpp.cpp

---
+++ Bénédict/checkcpp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <iomanip>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Das ist die Version " << __cplusplus << endl;
+
+//getchar();
+system("PAUSE");
+return 0;
+}
