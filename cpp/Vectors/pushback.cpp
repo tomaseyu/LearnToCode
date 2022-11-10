f528000bcb3a1fc939ca156cb1545e4d7806ee3d
radaxsly <72070675+tomaseyu@users.noreply.github.com>
2022-11-10 20:52:36 +0100

    Create pushback.cpp

---
+++ cpp/Vectors/pushback.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  
+  std::vector<std::string> last_jedi = {"kylo", "rey", "luke", "finn"};;
+  
+  // Add characters here:
+  last_jedi.push_back("darth maul");
+  
+  
+  
+  
+  
+  std::cout << last_jedi[0] << " ";
+  std::cout << last_jedi[1] << " ";
+  std::cout << last_jedi[2] << " ";
+  std::cout << last_jedi[3] << " ";
+  std::cout << last_jedi[4] << " ";
+  
+}
